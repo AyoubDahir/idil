03d032229ba472471f00750dbcef4db38f690779
Ayoub Dahir ali <69686810+AyoubDahir@users.noreply.github.com>
2025-02-15 15:40:35 +0300

    Update customer_sales.py

--- idil/models/customer_sales.py
+++ idil/models/customer_sales.py
@@ -37,7 +37,7 @@ class CustomerSaleOrder(models.Model):
         string="Currency",
         required=True,
         default=lambda self: self.env["res.currency"].search(
-            [("name", "=", "SOS")], limit=1
+            [("name", "=", "SL")], limit=1
         ),
         readonly=True,
     )
